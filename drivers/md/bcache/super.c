7ee7c6a59b40b4d9dd9c0f8a25d0ef938a3fe298
Gabriel de Perthuis <g2p.code@gmail.com>
2013-08-27 00:26:49 +0200

    bcache: register_quiet should still report errors
    register_quiet should still report all errors except
    "device already registered".
    
    Before this some potential errors would be silenced.
    
    Signed-off-by: Gabriel de Perthuis <g2p.code@gmail.com>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -1925,10 +1925,13 @@ static ssize_t register_bcache(struct kobject *k, struct kobj_attribute *attr,
 	if (IS_ERR(bdev)) {
 		if (bdev == ERR_PTR(-EBUSY)) {
 			bdev = lookup_bdev(strim(path));
-			if (!IS_ERR(bdev) && bch_is_open(bdev))
+			if (!IS_ERR(bdev) && bch_is_open(bdev)) {
+				if (attr == &ksysfs_register_quiet)
+					goto out;
 				err = "device already registered";
-			else
+			} else {
 				err = "device busy";
+			}
 		}
 		goto err;
 	}
@@ -1966,8 +1969,7 @@ out:
 err_close:
 	blkdev_put(bdev, FMODE_READ|FMODE_WRITE|FMODE_EXCL);
 err:
-	if (attr != &ksysfs_register_quiet)
-		pr_info("error opening %s: %s", path, err);
+	pr_info("error opening %s: %s", path, err);
 	ret = -EINVAL;
 	goto out;
 }
